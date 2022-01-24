2837d8cc8dc1f518390b30b609806a3da80add9f
slarkin252 <93968406+slarkin252@users.noreply.github.com>
2022-01-24 16:56:21 -0600

    LungSoundProcessingv1.m

--- LungSoundProcessing.m
+++ LungSoundProcessing.m
@@ -1,1 +1,13 @@
 #Processing Lung Sounds
+
+filename = 'LungSoundsAbbr//BP1_Asthma,I E W,P L L,70,M.wav';
+[y,fs] = audioread(filename);
+
+info = audioinfo(filename)
+
+y = y(:,1);
+    dt = 1/fs;
+    t = 0:dt:(length(y)*dt)-dt;
+    plot(t,y); xlabel('Seconds'); ylabel('Amplitude');
+##    figure
+##    plot(psd(spectrum.periodogram,y,'Fs',fs,'NFFT',length(y)));
