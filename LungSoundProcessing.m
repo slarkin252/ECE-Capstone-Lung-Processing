1659fc011357abb69c37aeb1ee924aaed9df7e00
slarkin252 <93968406+slarkin252@users.noreply.github.com>
2022-04-08 13:44:59 -0500

    Add files via upload

--- LungSoundProcessing.m
+++ LungSoundProcessing.m
@@ -6,10 +6,18 @@ cd 'D:/LungSounds/ECE-Capstone-Lung-Processing'; #maps to my dir, change to wher
 myfolder1 = 'LungSoundsAbbr/*.wav'; #searches the lung sound library, change to match variable l
 myfolderinfo = dir(myfolder1);
 l = 'LungSoundsAbbr/'; #folder name for specific lung sounds in use, can be changed to search different library folders
-mfin = myfolderinfo(3).name; #change this number to select file
-filename = strcat(l, mfin); #combines the folder name and file name since dir is wider than specific file
+l2 = 'FrequencyResponse/';
+l3 = 'ActualLungSounds/';
+#mfin = ;
+#myfolderinfo.name #can be used to get an output of all files in myfolderinfo in the correct order
+mfin = myfolderinfo(31).name #change this number to select file
+recordedSigs = 'Test500Hz523mVv2.wav';
+recordedLung = 'Test-3-29-2.wav';
+#filename = strcat(l, mfin); #combines the folder name and file name since dir is wider than specific file
+#filename = strcat(l2, recordedSigs); #used for the newly recorded signals
+filename = strcat(l3, recordedLung);#used to pull from different directory
 
-[x,fs] = audioread(filename);
+[x,fs] = audioread(filename);#reads our audio file into a graphable format
 #info = audioinfo(filename(2,1))#prints audiofile details to console
 
 xlen = length(x);                   
@@ -35,14 +43,21 @@ Xamp = 20*log10(sqrt(PS)*sqrt(2)); #Not really sure but probably some normalizat
 
 
 x = x(:,1);
+    figure
     dt = 1/fs;
     t = 0:dt:(length(x)*dt)-dt;
     plot(t,x,'r'); xlabel('Seconds'); ylabel('Amplitude');
+    grid on; xlim([0 10]); ylim([-.1 .1]);#beautifying the graph
     
     figure
     plot(fX, Xamp)
     grid on; xlim([0 max(fX)]); ylim([min(Xamp)-10 max(Xamp)+10]);#beautifying the graph 
-    view(-90, 90); xlabel('Frequency, Hz'); ylabel('Magnitude, dB');#^^^
+    xlabel('Frequency, Hz'); ylabel('Magnitude, dB');#^^^
+    
+##    figure
+##    xlabel('Frequency, Hz');ylabel('Amplitude');
+##    xlim([0 max(fX)]);
+##    plot(fX, x)
     
 ##    figure
 ##    plot(psd(spectrum.periodogram,y,'Fs',fs,'NFFT',length(y)));
